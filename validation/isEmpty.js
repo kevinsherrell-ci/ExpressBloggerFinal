a38151fc0cec57476d835b133f03ce28bc58c1f3
kevinsherrell <k.sherrell.1028@gmail.com>
2022-10-04 23:51:03 -0500

    done

--- validation/isEmpty.js
+++ validation/isEmpty.js
@@ -1,10 +1,12 @@
-const isEmpty = (data)=>{
+const isEmpty = (data) => {
     return (
+
         data === null ||
         data === "undefined" ||
         typeof data === "string" && data.trim() === "" ||
         Array.isArray(data) && data.length === 0 ||
         typeof data === "object" && Object.keys(data).length === 0
+
     )
 };
 
